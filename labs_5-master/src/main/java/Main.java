1904727f332b782d45f556744b10123bfbed6122
Kirill-Kaprizov <kkaprizov@gmail.com>
2025-05-26 01:44:52 +0600

    Add files via upload

---
+++ labs_5-master/src/main/java/Main.java
@@ -0,0 +1,17 @@
+    import phones.FindPhone;
+    import phones.Phone;
+    import phones.PhoneEx;
+
+    import java.awt.*;
+    import java.sql.SQLOutput;
+
+    public class Main {
+        public static void main(String[] args) {
+
+            Phone ph1=new Phone("okii" ,4  ,1550, Color.RED,16);
+            System.out.println(ph1);
+
+            PhoneEx ph2=new PhoneEx();
+            System.out.println(ph2);
+        }
+    }
